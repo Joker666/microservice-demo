32a2db0aa7e9d85377acfce0274760f11ba800bb
Hasan Rafi <mr.k779@outlook.com>
2020-11-13 18:22:45 +0600

    Created go package for protos

--- build.sh
+++ build.sh
@@ -5,3 +5,14 @@ set -e
 python -m grpc_tools.protoc --proto_path=./protos/project \
         --python_out=./projectService/proto \
         --grpc_python_out=./projectService/proto ./protos/project/*.proto
+
+protoc -I . -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
+    --go_out=. --go_opt=paths=source_relative \
+    --go-grpc_out=. --go-grpc_opt=paths=source_relative \
+    protos/api/api.proto
+
+protoc -I . -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
+     --grpc-gateway_out . \
+     --grpc-gateway_opt logtostderr=true \
+     --grpc-gateway_opt paths=source_relative \
+     protos/api/api.proto
--- projectService/proto/service_pb2_grpc.py
+++ projectService/proto/service_pb2_grpc.py
@@ -2,7 +2,7 @@
 """Client and server classes corresponding to protobuf-defined services."""
 import grpc
 
-from . import service_pb2 as service__pb2
+import service_pb2 as service__pb2
 
 
 class ProjectSvcStub(object):
